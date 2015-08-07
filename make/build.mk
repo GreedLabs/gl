0996198f60ffe16a88c9aa76a1f89587d991ba03
Greed <renaud.gaubert@gmail.com>
2015-08-07 22:29:05 +0200

    Build system now also outputs to a bin folder at `root` dir

--- make/build.mk
+++ make/build.mk
@@ -2,8 +2,8 @@ OBJ = $(addprefix $(BUILD_DIR)/, $(SRC:.cc=.o))
 
 all: $(TARGET)
 
-$(TARGET): $(OBJ) $(DIST_OBJ)
-	$(LINK.c) -o $(TARGET) $^ $(LDLIBS)
+$(TARGET): $(OBJ) $(DIST_OBJ) | bin
+	$(LINK.c) -o $(TARGET) -o $(ABS_PATH)/bin/$(TARGET) $^ $(LDLIBS)
 
 $(BUILD_DIR)/%.o: $(SRC_DIR)/%.cc
 	mkdir -p $(@D)
@@ -12,7 +12,12 @@ $(BUILD_DIR)/%.o: $(SRC_DIR)/%.cc
 $(DIST_BLD_DIR)/%.o: $(DIST_SRC_DIR)/%.cc
 	mkdir -p $(@D)
 	$(COMPILE.c) -o $@ $^
+
+bin:
+	mkdir -p $(ABS_PATH)/bin
+
 clean:
 	$(RM) $(OBJ) $(DIST_OBJ)
 	$(RM) $(TARGET)
 	$(RM) -r $(BUILD_DIR)
+	$(RM) -r $(ABS_PATH)/bin
