690ba5288069d61ce106e7977d59722f58ebcaa3
Renaud Gaubert <gauber_r@epita.fr>
2015-08-18 18:41:56 +0200

    Fixed symlinks (now uses fullpath)

--- make/build.mk
+++ make/build.mk
@@ -6,8 +6,8 @@ $(TARGET): $(OBJ) $(DIST_OBJ) | bin
 	$(LINK.c) -o $(TARGET) $^ $(LDLIBS)
 	mkdir -p $(ABS_PATH)/bin/$(TARGET)
 	cp $(TARGET) $(ABS_PATH)/bin/$(TARGET)/
-	([ -e ./shaders ] && ln -s ./shaders $(ABS_PATH)/bin/$(TARGET)/) || true
-	([ -e ./assets ] && ln -s ./assets $(ABS_PATH)/bin/$(TARGET)/) || true
+	([ -e ./shaders ] && ln -s $(shell pwd)/shaders $(ABS_PATH)/bin/$(TARGET)/shaders) || true
+	([ -e ./assets ] && ln -s $(shell pwd)/assets $(ABS_PATH)/bin/$(TARGET)/assets) || true
 
 $(BUILD_DIR)/%.o: $(SRC_DIR)/%.cc
 	mkdir -p $(@D)
